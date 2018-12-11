ed89c375478d81361d9c2f3aa9a806159f81ec0a
Marc Hermon <hermonm@urbandaleschools.com>
2018-12-11 08:18:25 -0600

    Create take home test

---
+++ 2.0_Take_Home_Test.py
@@ -0,0 +1,130 @@
+'''
+HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
+but I will definitely not ask another person except the instructor. Signed: ______________________
+
+1.) How do you enter a single line comment in a program?
+'''
+
+
+
+
+'''
+2.) What do the following lines of code output? ALSO: Why do they give a different answer?
+2 / 3
+2 // 3
+3/2
+3//2
+'''
+
+
+
+'''
+3.) What happens when you try this and why?
+A = 22
+b = 13
+c = a+b
+'''
+
+
+
+
+'''
+4.) All of the variable names below can be used. But which ONE of these is the better variable name to use?
+a
+A
+Area
+AREA
+area
+area_of_rectangle
+Area_Of_Rectangle
+'''
+
+
+
+'''
+5.) Which of these variables names are NOT allowed in Python? 
+Test them if you aren't sure.
+
+apple
+Apple
+APPLE
+Apple2
+1Apple
+account number
+account_number
+account.number
+accountNumber
+account#
+pi
+PI
+fred
+Fred
+GreatBigVariable
+greatBigVariable
+great_big_variable
+great.big.variable
+2x
+x2x
+total%
+#left
+'''
+
+
+
+
+'''
+6.) Predict the output of (a) and its type and then test it?
+a =2
+a*=10
+a/=2
+a+=12
+a-=7
+a
+type(a)
+'''
+
+
+
+
+
+'''
+7.) This program runs, but the code still could be better. How?
+radius = 10
+x = 3.14
+pi = x
+area = pi  * radius ** 2
+area
+'''
+
+
+
+
+'''
+8.) What is the ouput of each of these?
+type(42)
+type(42.0)
+type("C3PO")
+type(True)
+'''
+
+
+
+
+
+'''
+9.) Fix the mistake in the following code:
+x = 4
+y = 5
+a = 3(x + y)
+a
+'''
+
+
+
+
+'''
+10.)Why does this code not calculate the average?
+x,y,z =(3,4,5)
+ave = x+y+z/3
+ave
+'''
