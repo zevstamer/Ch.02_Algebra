155cde1978b71478a9f970f9ac7815a02b467e6b
Marc Hermon <hermonm@urbandaleschools.com>
2019-02-19 22:25:15 -0600

    Update 2.0_Take_Home_Test.py

--- 2.0_Take_Home_Test.py
+++ 2.0_Take_Home_Test.py
@@ -7,98 +7,13 @@ but I will definitely not ask another person except the instructor. Signed: ____
 
 
 
-2.) What do the following lines of code output? ALSO: Why do they give a different answer?
-2 / 3
-2 // 3
-3/2
-3//2
+2.) What is the output of each line of the following if b=5 and a=2
 
-
-
-
-
-3.) What happens when you try this and why?
-A = 22
-b = 13
-c = a+b
-
-
-
-
-
-
-4.) All of the variable names below can be used. But which ONE of these is the better variable name to use?
-a
-A
-Area
-AREA
-area
-area_of_rectangle
-Area_Of_Rectangle
-
-
-
-
-
-5.) Which of these variables names are NOT allowed in Python? 
-Test them if you aren't sure.
-
-apple
-Apple
-APPLE
-Apple2
-1Apple
-account number
-account_number
-account.number
-accountNumber
-account#
-pi
-PI
-fred
-Fred
-GreatBigVariable
-greatBigVariable
-great_big_variable
-great.big.variable
-2x
-x2x
-total%
-#left
-
-
-
-
-
-
-6.) Predict the output of (a) and its type and then test it?
-a =2
-a*=10
-a/=2
-a+=12
-a-=7
-a
-type(a)
-
-
-
-
-
-
-
-7.) This program runs, but the code still could be better. How?
-radius = 10
-x = 3.14
-pi = x
-area = pi  * radius ** 2
-area
-
-
-
-
-
-
-8.) What is the output of each of these?
+b/a
+b//a
+b**a
+b%a
+a+B
 type(42)
 type(42.0)
 type("C3PO")
@@ -106,13 +21,21 @@ type(True)
 
 
 
+3.) What is the final output of (a) and its type?
+
+a=2
+a*=10
+a/=2
+a+=12
+a-=7
+
+a
+type(a)
 
 
 
-
-9.) Fix the mistake in the following code:
-x = 4
-y = 5
+4.) Fix the mistake in the following code.
+x,y = (4,5)
 a = 3(x + y)
 a
 
@@ -121,7 +44,7 @@ a
 
 
 
-10.)Why does this code not calculate the average?
+5.) Fix the code so it will calculate the average.
 x,y,z =(3,4,5)
 ave = x+y+z/3
 ave
