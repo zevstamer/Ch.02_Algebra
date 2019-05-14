0ffb03533cab9f17891b86e93653a0df4fd97718
Marc Hermon <hermonm@urbandaleschools.com>
2019-05-14 15:18:44 -0500

    Update and rename 2.0_Take_Home_Test.py to 2.0_Jedi_Training.py

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,6 +1,5 @@
 '''
-HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
-but I will definitely not ask another person except the instructor. Signed: ______________________
+Sign your name:________________
 
 1.) How do you enter a single line comment in a program?
 
