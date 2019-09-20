5118a60b3aa616fe6442f2a7127a97e5d2186a1f
Marc Hermon <hermonm@urbandaleschools.com>
2019-09-20 06:23:01 -0500

    Update 2.0_Jedi_Training.py

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,13 +1,11 @@
 '''
 Sign your name:________________
 
-1.) How do you enter a single line comment in a program?
-
-
+1.) How do you enter a single line comment in a program? Give an example.
 
 
 2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
-What are the outputs?
+What are the outputs? If the output is an error record the error and try to determine what the error is!
 
 b/a                 =
 b//a                =
@@ -20,7 +18,6 @@ type("C3PO")        =
 type(True)          =
 
 
-
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
 into the Python Console Window?
 
@@ -29,29 +26,22 @@ a*=10
 a/=2
 a+=12
 a-=7
-
 a             =
 type(a)       =
 
 
+4.) What is the mistake in the following code. Fix it!
 
-4.) Fix the mistake in the following code.
 x,y = (4,5)
 a = 3(x + y)
 a
 
 
+5.) What is the mistake in the following code so it will calculate the average. Fix it!
 
-
-
-
-5.) Fix the code so it will calculate the average.
 x,y,z =(3,4,5)
 ave = x+y+z/3
 ave
-
-
-
 
 
 '''
